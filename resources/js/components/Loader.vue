d806baedf165507524167662a3093be048800b4a
Daniel <angelicodaniel93@gmail.com>
2023-02-01 16:21:04 +0100

    loader

--- resources/js/components/Loader.vue
+++ resources/js/components/Loader.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <div class="spinner-grow text-secondary" role="status">
+    <div id="loader">
+        <div class="spinner-grow text-primary" role="status">
             <span class="visually-hidden">Loading...</span>
         </div>
     </div>
@@ -11,3 +11,18 @@ export default {
     name: 'Loader'
 }
 </script>
+
+
+<style scoped lang="scss">
+    #loader{
+        background-color: gray;
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
