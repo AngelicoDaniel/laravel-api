769f32d22cf61de9f73fd32ee864f2a464ec1e49
Daniel <angelicodaniel93@gmail.com>
2023-02-01 18:32:47 +0100

    last commit

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -16,7 +16,7 @@ class PostController extends Controller
     public function index()
     {
         // $posts = Post::all();
-        $posts = Post::OrderBy('id', 'DESC')->with('category', 'tags')->paginate(5);
+        $posts = Post::OrderBy('id', 'DESC')->with('category', 'tags')->paginate(10);
         return response()->json($posts);
     }
 
